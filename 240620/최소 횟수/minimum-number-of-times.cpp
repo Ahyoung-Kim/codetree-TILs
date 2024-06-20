30e8d30c021875bf6a9e833bf9f97bd91e1c3f16
Ahyoung-Kim <79265861+Ahyoung-Kim@users.noreply.github.com>
2024-06-20 14:57:57 +0900

    Title: 최소 횟수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240620/최소 횟수/minimum-number-of-times.cpp
+++ 240620/최소 횟수/minimum-number-of-times.cpp
@@ -1,54 +1,46 @@
 #include <iostream>
 #include <deque>
-#include <cstring>
 
-using namespace std;
+#define MAX 100001
 
-#define MAX_SIZE 100001
+int a, b;
+int vis[MAX];
 
-int n, k;
-int visited[MAX_SIZE];
-
-// 최단 거리를 찾는 bfs 함수입니다.
 int bfs() {
     deque<int> dq;
-    dq.push_back(n);
-    visited[n] = 1;
 
-    while (!dq.empty()) {
-        int cur = dq.front();
-        dq.pop_front();
+    dq.push_back(a);
+    vis[a] = 1;
 
-        // b에 도달한 경우 해당 거리를 반환합니다.
-        if(cur == k) return visited[k] - 1;
+    while(!dq.empty()) {
+        int curr = dq.front(); dq.pop_front();
 
-        // 2번 연산의 경우, 2 * cur의 위치로 이동 가능합니다.
-        // 2번 연산은 몇번을 해도 상관 없으므로 덱의 앞쪽에 추가합니다.
-        if (cur * 2 < MAX_SIZE && !visited[cur * 2]) {
-            dq.push_front(cur * 2);
-            visited[cur * 2] = visited[cur];
+        if(curr == b) return vis[b] - 1;
+
+        for(int i = 0; i < 2; i++) {
+            int next = curr;
+
+            if(i == 0) next += 1;
+            else if(i == 1) next -= 1;
+            
+            if(next < MAX && !vis[next]) {
+                dq.push_back(next);
+                vis[next] = vis[curr] + 1;
+            }
         }
 
-        // 1번 연산은 cur + 1 또는 cur - 1로 이동합니다.
-        // 1번 연산은 덱의 뒤쪽에 추가합니다.
-        if (cur + 1 < MAX_SIZE && !visited[cur + 1]) {
-            dq.push_back(cur + 1);
-            visited[cur + 1] = visited[cur] + 1;
-        }
-
-        if (cur - 1 >= 0 && !visited[cur - 1]) {
-            dq.push_back(cur - 1);
-            visited[cur - 1] = visited[cur] + 1;
+        if(curr * 2 < MAX && !vis[curr * 2]) {
+            dq.push_front(curr * 2);
+            vis[curr * 2] = vis[curr];
         }
     }
 }
 
 int main() {
-    // 시작점과 도착점을 입력받습니다.
-    cin >> n >> k;
+    // 여기에 코드를 작성해주세요.
+    cin >> a >> b;
 
-    // bfs 함수를 호출한 결과를 출력합니다.
     cout << bfs() << endl;
-    
+
     return 0;
 }
